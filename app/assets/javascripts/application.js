84953f54b4eb22fd911d305cba806c03e0303072
Michael Rajca <rajcamichael@gmail.com>
2015-07-24 23:17:56 +0000

    made changes to application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -13,5 +14,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require_tree .
